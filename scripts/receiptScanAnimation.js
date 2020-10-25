fac37b35b621c090132fa9a5cd99ec5ba79ffede
philison <philison.dev@gmail.com>
2020-10-25 18:42:26 +0100

    coding of the text-gets-scanned effect

--- scripts/receiptScanAnimation.js
+++ scripts/receiptScanAnimation.js
@@ -9,15 +9,16 @@ for (var i = 0; i <= 8; i++) {
 
 let scanOptions = {
     root: null,
-    rootMargin: "-70px",
+    rootMargin: "0px 0px 0px 0px",
     threshold: [0.25, 0.5, 0.75, 1.0]
   };
 
 let phoneTextWrapper = document.querySelector('.phoneTextWrapper')
 let scanTextLines = document.querySelectorAll('.scanTextLine')
 
-let scanContainer = document.querySelector('.scanContainer')
-let receipt = document.querySelector('.receipt')
+//let scanContainer = document.querySelector('.scanContainer')
+//let receipt = document.querySelector('.receipt')
+let intersectDiv = document.querySelector('.intersectDiv')
 
 function receiptScanAnimation() {
     console.log('Test')
@@ -27,16 +28,40 @@ function receiptScanAnimation() {
 function createScanObserver() {
     let scanObserver = new IntersectionObserver(handleScanIntersect, scanOptions)
     //scanObserver.observe(phoneTextWrapper)
-    scanObserver.observe(receipt)
+    scanObserver.observe(intersectDiv)
 }
 
 function handleScanIntersect(entries, observer) {
     entries.forEach((entry) => {
         let intersect = entry.intersectionRatio;
-        console.log(entry.target)
+        //console.log(entry.target)
         console.log(intersect)
+
         phoneTextWrapper.style.transform = `translateY(${(1-intersect)*15}rem)`
-    
+        
+        scanTextLines.forEach((value, index) => {
+            if (index < scanTextLines.length*intersect) {
+                console.log(index, intersect, scanTextLines.length*intersect)
+                value.classList.add("textIsScanned")
+            } else {
+                value.classList.remove("textIsScanned")
+            }
+        })
+
+        /*
+        if (intersect = 1) {
+            
+        } else if(intersect >= 0.75) {
+            
+        } else if(intersect >= 0.5) {
+            
+        } else if(intersect >= 0.25) {
+            
+        } else {
+
+        }
+        */
+
         /*
         if ( intersect >= 0.33 && intersect < 0.66 ) {
             phoneTextWrapper.style.transform = 'translateY(10rem)'
