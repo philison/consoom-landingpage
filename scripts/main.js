08b8e3c836029f4305b8cc81e9b81d8339979be6
philison <philison.dev@gmail.com>
2020-10-25 13:27:48 +0100

    testing the scanIntersectObserver

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,56 @@
+import receiptScanAnimation from "./receiptScanAnimation.js";
 
 // variables
-let cardElement
+let cardElements
+let headeriPhone
+
+let slideInOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5
+  };
 
 window.addEventListener("load", (event) => {
-    cardElement = document.querySelector(".card");
+    cardElements = document.querySelectorAll(".card");
+    headeriPhone = document.querySelector(".mockupContainer")
+    //headeriPhone = document.getElementById("headeriPhone");
+    
   
-    createObserver();
+    createCardObservers();
+    createiPhoneObserver();
+    receiptScanAnimation();
   }, false);
 
 
-function createObserver() {
-    let observer;
-  
-    let options = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.5
-    };
-  
-    observer = new IntersectionObserver(handleIntersect, options);
-    observer.observe(cardElement);
+
+
+function createCardObservers() {
+    let cardObserver;
+
+    cardObserver = new IntersectionObserver(handleSlideInIntersect, slideInOptions);
+    cardElements.forEach((value) => {cardObserver.observe(value)})
+    //observer.observe(cardElement);
 }
 
-function handleIntersect(entries, observer) {
+function createiPhoneObserver() {
+    let headeriPhoneObserver;
+
+    headeriPhoneObserver = new IntersectionObserver(handleSlideInIntersect, slideInOptions)
+    headeriPhoneObserver.observe(headeriPhone)
+}
+
+function handleSlideInIntersect(entries, observer) {
     entries.forEach((entry) => {
         if (entry.intersectionRatio >= 0.5) {
-            console.log('card is visible')
+            //console.log('target is visible')
 
             entry.target.classList.add('fadedIn')
         } else {
-            console.log('card is not completely visible')
+            //console.log('target is not completely visible')
             entry.target.classList.add('fadedOut')
             entry.target.classList.remove('fadedIn')
         }
 
     });
   }
+
---
+++ scripts/receiptScanAnimation.js
@@ -0,0 +1,46 @@
+//options
+
+/*
+var thresholdList = [];
+for (var i = 0; i <= 8; i++) {
+    thresholdList.push(i*);
+}
+*/
+
+let scanOptions = {
+    root: null,
+    rootMargin: "300px",
+    threshold: [0.25, 0.5, 0.75, 1.0]
+  };
+
+let phoneTextWrapper = document.querySelector('.phoneTextWrapper')
+let scanTextLines = document.querySelectorAll('.scanTextLine')
+
+function receiptScanAnimation() {
+    console.log('Test')
+    createScanObserver()
+}
+
+function createScanObserver() {
+    let scanObserver = new IntersectionObserver(handleScanIntersect, scanOptions)
+    scanObserver.observe(phoneTextWrapper)
+}
+
+function handleScanIntersect(entries, observer) {
+    entries.forEach((entry) => {
+        let intersect = entry.intersectionRatio;
+        console.log(intersect)
+        phoneTextWrapper.style.transform = `translateY(${(1-intersect)*15}rem)`
+    
+        /*
+        if ( intersect >= 0.33 && intersect < 0.66 ) {
+            phoneTextWrapper.style.transform = 'translateY(10rem)'
+        } else if (intersect >= 0.66 && intersect < 1.0){
+            phoneTextWrapper.style.transform = 'translateY(5rem)'
+        } else if(intersect == 1.0) {
+            phoneTextWrappe
+            */
+    });
+}
+
+export default receiptScanAnimation;
